0e3a8bab95fd6b9ed9362d369e697e36f17cad9e
basti <bastian_behrendt@yahoo.de>
2024-04-15 12:22:42 +0100

    Initial commit

---
+++ src/yep.ts
@@ -0,0 +1,52 @@
+/*
+
+//1
+const age:number = 10;
+console.log(age);
+
+//2
+for (let i :number = 1; i <= age; i++ ){
+    console.log(i);
+}
+
+//3
+if(age > 18){
+    console.log("Erwachsen");
+}else {
+    console.log("Baby")}
+ */
+
+//4
+/*let score:number = 0;
+
+//5
+if(score <= 1){
+    console.log("Score is available");
+}else{
+    console.log("no value");
+}
+*/
+
+let score: any;
+if (score) {
+    console.log("Yep");
+}
+
+let username: string = "";
+
+if (username) {
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+let isAdmin: boolean = false;
+
+if (isAdmin) {
+    console.log(isAdmin);
+}
+
+if (!isAdmin) {
+    console.log(isAdmin);
+}
+
