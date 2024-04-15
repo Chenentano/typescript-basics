fd73171cd433fab8bf92660f2546f5d153e661c7
basti <bastian_behrendt@yahoo.de>
2024-04-15 15:03:06 +0100

    arrow funcs

--- src/yep.ts
+++ src/yep.ts
@@ -1,52 +1,15 @@
-/*
+const numbers:number[] = [1,2,4,5,6];
+const multi = (numbers.map(numbers => numbers * 2));
+console.log("multi Func: " + multi);
 
-//1
-const age:number = 10;
-console.log(age);
+const words:string[] = ["hi", "hallo","test","willkommen","soissesneee"];
+const longerThanFive = words.filter(words => words.length > 5);
+console.log("longerThanFive Func: " + longerThanFive);
 
-//2
-for (let i :number = 1; i <= age; i++ ){
-    console.log(i);
-}
+const newNumbers:number[] = [7,8,9,10,11];
+const calcNumbers = newNumbers.reduce((oldSum, newSum) => oldSum + newSum,0);
+console.log("calcNumbers Func: " + calcNumbers);
 
-//3
-if(age > 18){
-    console.log("Erwachsen");
-}else {
-    console.log("Baby")}
- */
-
-//4
-/*let score:number = 0;
-
-//5
-if(score <= 1){
-    console.log("Score is available");
-}else{
-    console.log("no value");
-}
-*/
-
-let score: any;
-if (score) {
-    console.log("Yep");
-}
-
-let username: string = "";
-
-if (username) {
-    console.log(true);
-}else{
-    console.log(false);
-}
-
-let isAdmin: boolean = false;
-
-if (isAdmin) {
-    console.log(isAdmin);
-}
-
-if (!isAdmin) {
-    console.log(isAdmin);
-}
-
+const evenNewerNumbersHoly:number[] = [13,1,4,15,6];
+const checkIfGreaterThan10 = evenNewerNumbersHoly.some(evenNewerNumbersHoly => evenNewerNumbersHoly > 10);
+console.log("checkIfGreaterThan10 Func: " + checkIfGreaterThan10);
